72a7d462ac52b1a906a2d680ee7ce4a1b596dac1
Junus Ergin <junus91@googlemail.com>
2024-05-08 18:36:45 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div id="app">
-    <!-- Weitere Komponenten oder Elemente können hier eingefügt werden -->
-  </div>
+  <BaseCard>
+    <h1>Überschrift</h1>
+    <p>Dies ist ein Beispieltext, der in die Card-Komponente eingefügt wird.</p>
+  </BaseCard>
+
+
 </template>
 
 <script>
+import BaseCard from './components/BaseCard.vue';
+
 export default {
   name: 'App',
   components: {
+    BaseCard
   }
 }
 </script>
@@ -15,11 +21,14 @@ export default {
 <style>
 body {
   margin: 0;
+
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <!-- Slot für beliebigen Inhalt -->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard'
+}
+</script>
+
+<style scoped>
+.card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+}
+</style>
