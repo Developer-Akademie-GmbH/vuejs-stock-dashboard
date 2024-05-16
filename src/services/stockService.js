e4837a7c700d2a2db43fb8f5cd083c387650f5ca
Junus Ergin <junus91@googlemail.com>
2024-05-16 11:44:57 +0200

    09  - Werte richtig sortieren

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
+const order = [
+  'Mar 21',
+  'Jun 21',
+  'Sep 21',
+  'Dec 21',
+  'Mar 22',
+  'Jun 22',
+  'Sep 22',
+  'Dec 22',
+  'Mar 23',
+  '""',
+  '3 Aug 23',
+  '2 Nov 23',
+  '1 Feb 24',
+  '2 Mai 24'
+];
+
 class StockService {
   constructor() {
     this.apiClient = axios.create({
@@ -15,6 +32,11 @@ class StockService {
       console.error('Error fetching data:', error);
       return [];
     }
+  }
+
+  async getRevenue(sheetName) {
+    const data = await this.fetchData(sheetName)
+    return order.map(key => data[3][key]);
   }
 }
 
